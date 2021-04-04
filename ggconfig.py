ce3de53f9965bd17ba11cab4a4a0c2bdb6f19d2c
ooz <ooz@users.noreply.github.com>
2021-04-04 21:28:40 +0200

    Update ggpy

--- ggconfig.py
+++ ggconfig.py
@@ -4,7 +4,6 @@
 config = {
     'site': {
         'base_url': 'https://oliz.io',
-        'render_root_readme': True,
         'generate_sitemap': True,
         'additional_sitemap_entries': [
             'https://oliz.io/blog/',
